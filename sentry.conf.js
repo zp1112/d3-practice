0cead173fc241eecf0515c0a225d08272f6a1203
Ping Zheng (Bo Yan Ke Ji Gu Fen You Xian G) <v-pizhen@microsoft.com>
2018-10-10 18:29:33 +0800

    emmm

--- sentry.conf.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-
-  // 此处可以从你的sentry.io的全路径看出，如：https://sentry.io/此处是你的org/此处是你的project
-    organization: 'sentry', 
-    project: 'react',
-    apiKey: '787b0eb98fea486283cf7d3ada7bdcf014ad925736e749529f90431ae262dd0d',
-  }
