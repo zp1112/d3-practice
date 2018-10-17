a67d6f977b70d578063c3fde26fb9e1f2d81eec6
Ping Zheng (Bo Yan Ke Ji Gu Fen You Xian G) <v-pizhen@microsoft.com>
2018-10-17 18:02:54 +0800

    polygon

--- src/container/zoom/example1/chart.js
+++ src/container/zoom/example1/chart.js
@@ -26,23 +26,27 @@ export default class Chart {
     this.scaleTime = d3
       .scaleTime()
       .domain(d3.extent(this.data, d => d.date))
-      .rangeRound([0, this.width]);
+      .range([0, this.width]);
 
     this.scaleLinear = d3
       .scaleLinear()
       .domain([0, d3.max(this.data, d => d.price)])
       .range([this.height, 0]);
-    this.svg = d3
+      this.container = d3
       .select(this.el)
+        .append('div')
+        // .attr("width", this.width + this.padding * 2)
+        .attr("height", this.height + this.padding * 2)
+        this.container.style(
+          "transform",
+          `translate(${(this.width + this.padding * 2) / 2}px, ${(this.height +
+            this.padding * 2) /
+            2}px)`
+            );
+    this.svg = this.container
       .append("svg")
       .attr("width", this.width + this.padding * 2)
       .attr("height", this.height + this.padding * 2)
-      .attr(
-        "transform",
-        `translate(${(this.width + this.padding * 2) / 2}, ${(this.height +
-          this.padding * 2) /
-          2})`
-      );
     this.defs = this.svg.append("defs");
     this.defs
       .append("clipPath")
@@ -110,14 +114,52 @@ export default class Chart {
       .y1(d => {
         return this.scaleLinear(d.price);
       });
-
-    this.timetickGroup
-      .append("path")
+    this.timetickGroup.append("path")
       .datum(this.data)
       .attr("class", "area")
+      .attr("id", "area")
       .style("fill", `url(#${this.gradientID})`)
       .attr("clip-path", `url(#${this.clipID})`)
-      .attr("d", this.area);
+      .attr("d", this.area)
+      .on("mouseover mousemove touchstart touchmove", hoverMouseOn);
+      this.timetickGroup
+      .append("line")
+      .attr("class", "topline")
+      .attr('stroke', 'black')
+      .attr('stroke-width', 5)
+      .attr("x1", 0)
+      .attr("y1", 0)
+      .attr("x2", this.width)
+      .attr("y2", 0);
+    // arearect.on("mouseover mousemove touchstart touchmove", hoverMouseOn);
+      const polygon = this.timetickGroup
+      .append("g")
+      .attr("class", "focusgroup");
+      polygon.append('polygon')
+			.attr("points", "0,0 16,0 8,4")
+			.style("fill", "#385e69")
+			.style("stroke", "#385e69");
+      polygon.append('line')
+      .attr("x1", 8)
+      .attr("y1", 0)
+      .attr("x2", 8)
+      .attr("y2", this.height)
+			.style("fill", "#385e69")
+      .style("stroke", "#385e69");
+
+      this.tooltip = this.container
+      .append("div").attr('class', 'tooltip')
+      function hoverMouseOn() {
+        $('.tooltip').hide()
+        const mouse_x = d3.mouse(this)[0];
+        const xPostion = (that.t ? that.t.rescaleX(that.scaleTime) : that.scaleTime).invert(mouse_x);
+        const focueGroup = d3.select('.focusgroup');
+        const focusGroupWidth = 16;
+        focueGroup.attr("transform", "translate(" + (mouse_x - focusGroupWidth * 0.5) + "," + 0 + ")");
+        d3.select('.tooltip').html(that.data.find(d => formatDate(d.date) === formatDate(xPostion)) ? that.data.find(d => formatDate(d.date) === formatDate(xPostion)).price : '无数据')
+        d3.select('.tooltip').style('transform', `translate(${that.padding}px, ${that.padding}px)`).style('left', mouse_x + 'px').style('top', '30px')
+        $('.tooltip').show();
+      }
 
     this.line = d3
     .line()
@@ -129,25 +171,43 @@ export default class Chart {
       .append('g')
     lineGroup.append('path')
       .attr('stroke', 'blue')
+      .attr('class', 'topline')
       .attr('fill', 'none')
       .style("stroke-width","4")
+      .attr("clip-path", `url(#${this.clipID})`)
       .attr('d', this.line(this.data));
     lineGroup.append('path')
       .attr('stroke', 'black')
       .attr('fill', 'none')
+      .attr('class', 'topline1')
       .style("stroke-dasharray", "3")
       .style("stroke-width","4")
+      .attr("clip-path", `url(#${this.clipID})`)
       .attr('d', this.line(this.data.filter(d => d.price !== 0)));
 
     const that = this;
     function zoomed() {
       var t = d3.event.transform,
         xt = t.rescaleX(that.scaleTime);
+        that.translatex = t.x;
+        that.t = t;
       that.timetickGroup.select(".area").attr(
         "d",
         that.area.x(function(d) {
           return xt(d.date);
         })
+      );
+      that.timetickGroup.selectAll(".topline").attr(
+        "d",
+        that.line.x(function(d) {
+          return xt(d.date);
+        })(that.data)
+      );
+      that.timetickGroup.selectAll(".topline1").attr(
+        "d",
+        that.line.x(function(d) {
+          return xt(d.date);
+        })(that.data.filter(d => d.price !== 0))
       );
       that.timetickGroup.select(".axis--x").call(
         that.axisX
@@ -159,4 +219,6 @@ export default class Chart {
       );
     }
   }
+
+  
 }
